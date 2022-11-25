8a141e3c467982bb005a30f39dd1c0cc6ae41464
Luiz Eduardo Tojal <64617480+luizsantos-1@users.noreply.github.com>
2022-11-25 20:35:25 -0300

    Create context class

--- ControleDeAcesso/Data/Context.cs
+++ ControleDeAcesso/Data/Context.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ControleDeAcesso.Data
 {
-    internal class Context
+    public class Context : DbContext
     {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(GetUsers());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static User[] GetUsers()
+        {
+            return new User[]
+            {
+            new User
+            {
+                Id = Guid.NewGuid(),
+                Username = "admin@email.com",
+                Password = "admin123"
+            }
+            
+            
+            };
+        }
     }
 }
