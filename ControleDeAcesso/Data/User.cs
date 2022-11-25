ec965ea9b9a1a7f62f27f34d1ff292ba800b0eb7
Luiz Eduardo Tojal <64617480+luizsantos-1@users.noreply.github.com>
2022-11-25 20:35:52 -0300

    Create User model

--- ControleDeAcesso/Data/User.cs
+++ ControleDeAcesso/Data/User.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ControleDeAcesso.Data
 {
-    internal class User
+    public class User
     {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
     }
 }
